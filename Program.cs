8010cc33552a154c228eac2c654ec2f15260d0d8
natthapon03 <pinnatthapon@gmail.com>
2023-04-24 10:50:45 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program{
+    static void Main(string[] args){
+        Console.Write("Pleas Input number");
+        string number1 = Console.ReadLine();
+
+        Console.Write("Pleas Input nameitem");
+        string nameitem = Console.ReadLine();
+
+        Console.Write("Pleas Input itemtype");
+        string itemtype = Console.ReadLine();
+        
+            
+        
+        
+            
+        
+
+
+    
+
+
+
+    }
+}
